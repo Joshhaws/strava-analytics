ea30529a81421c69a4f6ea600927a425950e522e
joshhaws <joshhaws0@gmail.com>
2025-01-17 17:21:15 -0700

    removing node and uncommited code

--- src/lib/db.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
-import { getDbConfig } from '../db/db-config.js';
-
-dotenv.config();
-
-const pool = new Pool(getDbConfig());
-
-export default pool;
--- src/server/db.js
+++
@@ -1,6 +0,0 @@
-import pg from 'pg';
-import { DB_CONFIG } from './config.js';
-
-const pool = new pg.Pool(DB_CONFIG);
-
-export default pool;
